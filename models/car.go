5c09ab530517d0775576c1ef7b0f7432eb0a301c
Patrickstvn <168065344+Patrickstvn@users.noreply.github.com>
2025-08-18 11:03:41 +0700

    Update car.go

--- models/car.go
+++ models/car.go
@@ -3,29 +3,32 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Car struct {
-	Id              primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	Images          []string           `json:"images"`
-	Preview         string             `json:"preview"`
-	Vehicle         string             `json:"vehicle"`
-	ModelYear       int                `json:"modelyear"`
-	ExteriorColour  string             `json:"exteriorcolour"`
-	InteriorColours string             `json:"interiorcolours"`
-	Wheels          string             `json:"wheels"`
-	Seats           string             `json:"seats"`
+	Id                    primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Images                []string           `json:"images"`
+	Preview               string             `json:"preview"`
+	Vehicle               string             `json:"vehicle"`
+	ModelYear             int                `json:"modelyear"`
+	ExteriorColour        string             `json:"exteriorcolour"`
+	InteriorColours       string             `json:"interiorcolours"`
+	Wheels                string             `json:"wheels"`
+	Seats                 string             `json:"seats"`
+	
+	PaintedWheels         string             `json:"paintedwheels"`
+	LetteringDecals       string             `json:"letteringdecals"`
+	SeatbeltsSeatdesign   string             `json:"seatbeltsseatdesign"`
+	ExteriorDesign        []string           `json:"exteriordesign"`
+	InteriorDesign        []string           `json:"interiordesign"`
+	AssistanceSystems     string             `json:"assistancesystems"`
+	ComfortNUsability     []string           `json:"comfortnusability"`
+	LightsVision          []string             `json:"lightsvision"`
+	EquipmentPackages     string             `json:"equipmentpackages"`
+	WheelColours          string             `json:"wheelcolours"`
+	WheelAccesories       string             `json:"wheelaccesories"`
 
-	PaintedWheels       string   `json:"paintedwheels"`
-	LetteringDecals     string   `json:"letteringdecals"`
-	SeatbeltsSeatdesign string   `json:"seatbeltsseatdesign"`
-	ExteriorDesign      []string `json:"exteriordesign"`
-	InteriorDesign      []string `json:"interiordesign"`
-	AssistanceSystems   []string `json:"assistancesystems"`
-	ComfortNUsability   []string `json:"comfortnusability"`
-	LightsVision        []string `json:"lightsvision"`
-	EquipmentPackages   string   `json:"equipmentpackages"`
 
-	RoofTransport         string   `json:"rooftransport"`
-	PowertrainPerformance []string `json:"powertrainperformance"`
-	Infotainment          string   `json:"infotainment"`
-	CommNr                string   `json:"commnr"`
-	Price                 int      `json:"price"`
+	RoofTransport         string             `json:"rooftransport"`
+	PowertrainPerformance []string           `json:"powertrainperformance"`
+	Infotainment          string             `json:"infotainment"`
+	CommNr                string             `json:"commnr"`
+	Price                 int                `json:"price"`
 }
