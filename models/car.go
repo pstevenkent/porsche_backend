5e13d0b0fa1c3fabe7376998317473c07578139c
Patrickstvn <168065344+Patrickstvn@users.noreply.github.com>
2025-08-18 09:42:06 +0700

    Update car.go

--- models/car.go
+++ models/car.go
@@ -12,6 +12,19 @@ type Car struct {
 	InteriorColours       string             `json:"interiorcolours"`
 	Wheels                string             `json:"wheels"`
 	Seats                 string             `json:"seats"`
+	
+	PaintedWheels         string             `json:"paintedwheels"`
+	LetteringDecals       string             `json:"letteringdecals"`
+	SeatbeltsSeatdesign   string             `json:"seatbeltsseatdesign"`
+	ExteriorDesign        []string           `json:"exteriordesign"`
+	InteriorDesign        []string           `json:"interiordesign"`
+	AssistanceSystems     string             `json:"assistancesystems"`
+	ComfortNUsability     []string           `json:"comfortnusability"`
+	LightsVision          []string             `json:"lightsvision"`
+	EquipmentPackages     string             `json:"equipmentpackages"`
+
+
+
 	RoofTransport         string             `json:"rooftransport"`
 	PowertrainPerformance []string           `json:"powertrainperformance"`
 	Infotainment          string             `json:"infotainment"`
